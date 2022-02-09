e1a4538d448f3b48b6b5f6dfc4fb9edff9eeb0f6
Ville Heikkilä <ville.heikkila@tuni.fi>
2022-02-09 15:57:22 +0200

    Updated installation files copy script

--- scripts/copy_platform_files.sh
+++ scripts/copy_platform_files.sh
@@ -11,6 +11,7 @@
 current_folder=$(pwd)
 script_folder=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 source_folder=$(echo "${script_folder}/Platform-Manager" | sed 's/scripts/src/')
+standalone_source_folder="${script_folder}/standalone"
 platform_folder=$(echo "${script_folder}" | sed 's/scripts/platform/')
 
 read -p "Do you want to install the simulation platform to the folder $platform_folder? [y/n] " answer
@@ -22,7 +23,8 @@ fi
 file_list_file="${source_folder}/platform_file_list.txt"
 
 copy_file() {
-    current_file=$1
+    current_source_folder=$1
+    current_file=$2
 
     if [[ ${current_file} == *"/"* ]]
     then
@@ -33,7 +35,7 @@ copy_file() {
         fi
     fi
 
-    cp ${source_folder}/${current_file} ${platform_folder}/${current_file}
+    cp ${current_source_folder}/${current_file} ${platform_folder}/${current_file}
 }
 
 echo "Copying the required platform files to ${platform_folder}"
@@ -56,15 +58,38 @@ do
     fi
 
     echo "Copying file ${required_file}"
-    copy_file "${required_file}"
+    copy_file ${source_folder} ${required_file}
 done < "${file_list_file}"
 
+standalone_file_list_file="${script_folder}/standalone_files.txt"
+echo "Replacing some platform files with standalone platform versions"
+
+if ! test -f "${standalone_file_list_file}"
+then
+    echo "Cannot find file '${standalone_file_list_file}'."
+    return 0 2> /dev/null || exit 0
+fi
+
+# fetch all the files marked as required files in the fetched file list
+while IFS= read -r standalone_file || [ -n "${standalone_file}" ]
+do
+    first_character=${standalone_file:0:1}
+    # check that the current line is not a comment or empty line
+    if [[ "${first_character}" == "#" ]] || [[ "${standalone_file}" == "" ]]
+    then
+        continue
+    fi
+
+    echo "Copying file ${standalone_file}"
+    copy_file ${standalone_source_folder} ${standalone_file}
+done < "${standalone_file_list_file}"
+
 echo ""
-echo "Checking if all required files have been copied."
+echo "Checking if all required files are available."
 
 # check that all required files are found
 check_all="true"
-while read -r required_file
+while IFS= read -r required_file || [ -n "${required_file}" ]
 do
     first_character=${required_file:0:1}
     # check that the current line is not a comment or empty line
@@ -80,11 +105,27 @@ do
     fi
 done < "${file_list_file}"
 
+while IFS= read -r required_file || [ -n "${required_file}" ]
+do
+    first_character=${required_file:0:1}
+    # check that the current line is not a comment or empty line
+    if [[ "${first_character}" == "#" ]] || [[ "${required_file}" == "" ]]
+    then
+        continue
+    fi
+
+    if ! test -f "${platform_folder}/${required_file}"
+    then
+        echo "File '${required_file}' was not found!"
+        check_all="false"
+    fi
+done < "${standalone_file_list_file}"
+
 if [[ "${check_all}" == "true" ]]
 then
     echo "All files where found. Platform is ready for installation."
 else
-    echo "There were missing files. Check ${file_list_file} for the required files."
+    echo "There were missing files. Check ${file_list_file} and ${standalone_file_list_file} for the required files."
 fi
 
 # set the current folder back to the original
